6a124cc0c350a9805510e464387880dc834fa34d
Viktoriia <pecourses@gmail.com>
2021-04-24 19:34:48 +0300

    next commit

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ let stringValue = "Hello, world!";
 console.log('stringValue =', stringValue);
 console.log('typeof stringValue :>> ', typeof stringValue);
 
-// Boolean (true, false)
+// boolean (true, false)
 let booleanValue = true;
 console.log("booleanValue =", booleanValue);
 console.log('typeof booleanValue :>> ', typeof booleanValue);
@@ -63,3 +63,50 @@ console.log('typeof symbolValue :>> ', typeof symbolValue);
 let myValue = 3-"fjjdskfhjk";
 console.log('myValue :>> ', myValue);
 console.log('typeof myValue :>> ', typeof myValue);
+
+//********************************************************* */
+// Операции и их приоритет
+let appleNumber = 4;
+// постфиксный инкремент (декремент)
+console.log('appleNumber-- :>> ', appleNumber--);
+console.log('appleNumber :>> ', appleNumber);
+
+let grapeNumber = 4;
+// префиксный инкремент (декремент)
+console.log('--grapeNumber :>> ', --grapeNumber);
+console.log('grapeNumber :>> ', grapeNumber);
+//appleNumber = appleNumber + 1;
+// appleNumber++; 
+
+let peopleNumber = 2;
+
+console.log('11 + 2 =', 11 + 2);
+console.log('11 - 2 =', 11 - 2);
+console.log('11 * 2 =', 11 * 2);
+console.log('11 / 2 =', 11 / 2);
+console.log('5 * 11 % 2 =', 11 % 2);
+// Операции: унарные, бинарные, тернарные
+
+// Приведение типов
+// Ввести число и вывести число на единицу больше
+let myValue2 = prompt("Input Number:");
+let newMyValue2 = Number(myValue2);
+newMyValue2++;
+alert("New number is " + newMyValue2);
+
+console.log('5 + "qwerty" = ', 5 + "qwerty");
+console.log('5 + null = ', 5 + null);
+console.log('5 + undefined = ', 5 + undefined);
+console.log('5 + true = ', 5 + true);
+//console.log('5 + Symbol() = ', 5 + Symbol());
+
+console.log('"qwerty" + "qwerty" = ', "qwerty" + "qwerty");
+console.log('"qwerty" + null = ', "qwerty" + null);
+console.log('"qwerty" + undefined = ', "qwerty" + undefined);
+console.log('"qwerty" + true = ', "qwerty" + true);
+// console.log('"qwerty" + Symbol() = ', "qwerty" + Symbol());
+
+// Неявное преобразование типа
+const myAge = prompt("Enter your age:");
+const myAgeNumber = +myAge;
+alert("You age" + myAgeNumber);
